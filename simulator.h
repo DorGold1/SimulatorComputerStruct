f7c42de894aa35107863c0cc592d67a8904f7c51
Dor Golfaiz <dorgolfaiz@mail.tau.ac.il>
2021-12-21 16:08:19 +0200

    added dmemin and imemin

--- simulator.h
+++ simulator.h
@@ -13,39 +13,9 @@ typedef struct instruction {
     short rm;
     int immediate1;
     int immediate2;
-}Instruction;
+} Instruction;
 
-
-typedef struct argument_registers {
-    int a0;
-    int a1;
-    int a2;
-} ArgRegs;
-
-typedef struct temporary_registers {
-    int t0;
-    int t1;
-    int t2;
-} TmpRegs;
-
-typedef struct saved_registers {
-    int s0;
-    int s1;
-    int s2;
-} SavedRegs;
-
-typedef struct registers {
-    ArgRegs arg;
-    TmpRegs tmp;
-    SavedRegs saved;
-    int zero;
-    int imm1;
-    int imm2;
-    int v0;
-    int gp;
-    int sp;
-    int ra;
-} Registers;
+int R[16];
 
 int read_instructions(FILE *fp, int len, Instruction **cmdLst);
 int add_to_cmd_lst(Instruction *cmdLst, char *inst);
